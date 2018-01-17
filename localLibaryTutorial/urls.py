f72042fe409cdfe78086e465ac5c217af8cebd66
Nikki Wong <NikkiWong@ecf.ie>
2018-01-17 15:07:09 +0000

    created a catalog app

--- localLibaryTutorial/urls.py
+++ localLibaryTutorial/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)), #Add URL maps to redirect the base URL to our application
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Use static() to add url mapping to serve static files during development (only)
