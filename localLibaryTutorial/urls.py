bf0d8bba2c16a68bcb53cd269cd0a49aaf1d9181
Nikki Wong <NikkiWong@ecf.ie>
2018-01-17 16:18:09 +0000

    created models for the catalog

--- localLibaryTutorial/urls.py
+++ localLibaryTutorial/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
 from django.conf import settings
+from django.urls import include
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    path('catalog/', include('catalog.urls')), # Use include() to add paths from the catalog application
     path('', RedirectView.as_view(url='/catalog/', permanent=True)), #Add URL maps to redirect the base URL to our application
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # Use static() to add url mapping to serve static files during development (only)
